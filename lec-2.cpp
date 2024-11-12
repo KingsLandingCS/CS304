de794696cab88269081096e680ac53ff65d81379
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-11-13 00:11:39 +0500

    learning

---
+++ lec-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+// #include <conio.h>
+using namespace std;
+
+void setA();
+void getA();
+void setA()
+{
+    a = 25;
+
+};
+
+class Student
+{
+private: // ===> data Hiding
+    int a;
+
+public:
+    void setA()
+    {
+        a = 25;
+    }
+    void getA()
+    {
+        cout << a << endl;
+    }
+};
+
+main()
+{
+
+    // getch();
+    return 0;
+}
