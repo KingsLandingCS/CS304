cf57f1ec1a4602567524e517dc0e5f753001abf5
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-10-14 02:10:35 +0500

    learning

---
+++ lec-5.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Woman
+{
+public:
+    void walk()
+    {
+        cout << "walk Function" << endl;
+    }
+};
+
+class Fish
+{
+public:
+    void swim()
+    {
+        cout << "swim Function" << endl;
+    }
+};
+
+class Mermaid : public Woman, public Fish
+{
+public:
+    // void callFun()
+    // {
+
+    // }
+};
+
+int main()
+{
+    Mermaid M;
+
+    M.walk();
+    M.swim();
+    return 0;
+}
