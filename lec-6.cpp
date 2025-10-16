a982c5b572957661513156d15f62b8c485e608a7
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-10-16 15:13:03 +0500

    learning

---
+++ lec-6.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+class Student
+{
+private:
+    string name, grade;
+    int age, roll_number;
+
+    // Function Getter and Setter
+
+public:
+    void setName(string s)
+    {
+        name = s;
+    }
+    void setAge(int s)
+    {
+        age = s;
+    }
+    void setRollNumber(int s)
+    {
+        roll_number = s;
+    }
+    void setGrade(string s)
+    {
+        grade = s;
+    }
+};
+
+int main()
+{
+    Student S1;
+
+    S1.setName("Ali");
+    S1.setAge(25);
+    S1.setRollNumber(52);
+    S1.setGrade("B");
+}
