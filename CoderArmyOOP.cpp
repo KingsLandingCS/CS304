f48159780601da7b5c2adb3866c0f537ad4ed3b9
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-10-31 06:18:49 +0500

    learning

---
+++ CoderArmyOOP.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class a
+{
+    // int b;
+    // char c;
+    // int d;
+};
+
+int main()
+{
+    a obj;
+    cout << sizeof(obj) << " " << endl;
+    return 0;
+}
