e768078cfd4822089a91e720894c89d414302be4
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-11-17 20:00:51 +0500

    learning

--- destructor-2.cpp
+++ destructor-2.cpp
@@ -8,6 +8,12 @@ class Customer
     int *data;
 
 public:
+    Customer()
+    {
+        name = "4";
+        cout << "Constructor is " << name << endl;
+    }
+
     Customer(string name)
     {
         this->name = name;
@@ -23,4 +29,6 @@ public:
 int main()
 {
     Customer A1("1"), A2("2"), A3("3");
+    Customer *A4 = new Customer;
+    delete A4;
 }
