154b01b1779f4a0338f9a10bc40b83b2d8d0ad40
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-11-20 05:18:28 +0500

    learning

--- Encapsulation.cpp
+++ Encapsulation.cpp
@@ -7,6 +7,7 @@ class Customer
     string name;
     int accountNumber, balance;
     static int totalCustomer;
+    static int totalBalance;
 
 public:
     Customer(string name, int accountNumber, int balance)
@@ -15,11 +16,31 @@ public:
         this->accountNumber = accountNumber;
         this->balance = balance;
         totalCustomer++;
+        totalBalance += balance;
     }
 
     static void accessStatic()
     {
-        cout << totalCustomer << endl;
+        cout << "Total number of Customer" << totalCustomer << endl;
+        cout << "Total Balance:" << totalBalance << endl;
+    }
+
+    void deposit(int amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+            totalBalance += amount;
+        }
+    }
+
+    void withdraw(int amount)
+    {
+        if (amount <= balance && amount > 0)
+        {
+            balance -= amount;
+            totalBalance -= amount;
+        }
     }
 
     void display()
@@ -34,14 +55,16 @@ public:
 };
 
 int Customer::totalCustomer = 0;
+int Customer::totalBalance = 0;
 
 int main()
 {
 
     Customer A1("Rohit", 1, 1000);
     Customer A2("Mohit", 2, 1800);
-    Customer A3("Ali", 3, 2300);
+    Customer A3("Ali", 3, 2000);
+    A1.deposit(800);
+    A2.withdraw(500);
     Customer::accessStatic();
-    A2.displayTotal();
     return 0;
 }
