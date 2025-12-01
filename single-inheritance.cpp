23e53a4c2dfd5a92b139b4838eebacc6eade9c71
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-12-02 01:18:52 +0500

    learning

---
+++ single-inheritance.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Human
+{
+protected:
+    string name;
+    int age;
+
+public:
+    void work()
+    {
+        cout << "I'm working " << endl;
+    }
+};
+
+class Student : public Human
+{
+    int rollNumber, fees;
+
+public:
+    Student(string name, int age, int rollNumber, int fees)
+    {
+        this->name = name;
+        this->age = age;
+        this->rollNumber = rollNumber;
+        this->fees = fees;
+    }
+};
+
+int main()
+{
+    Student A1("Rohit", 26, 32, 99);
+    A1.work();
+}
