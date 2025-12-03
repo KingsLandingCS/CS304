2189785e146ac0fee8ed315ebaee0e11b5a87a19
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-12-03 16:04:46 +0500

    learning

---
+++ MultipleInheritance.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class Engineer
+{
+public:
+    string specilization;
+
+    void work()
+    {
+        cout << " I have Specilization in " << specilization << endl;
+    }
+};
+
+class Youtuber
+{
+public:
+    int subscribers;
+
+    void contentCreator()
+    {
+        cout << " I have a subscriber base of " << subscribers << endl;
+    }
+};
+
+class CodeTeacher : public Engineer, public Youtuber
+{
+public:
+    string name;
+
+    CodeTeacher(string name, string specilization, int subscribers)
+    {
+        this->name = name;
+        this->specilization = specilization;
+        this->subscribers = subscribers;
+    }
+
+    void showcase()
+    {
+        cout << " My name is " << name << endl;
+        work();
+        contentCreator();
+    }
+};
+
+int main()
+{
+    CodeTeacher A1("Zubi", "Theory of AutoMata", 49000);
+    A1.showcase();
+}
