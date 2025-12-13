4f87719e5a4476d7fcb6ff6cde7a3b34cbd84162
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2025-12-13 21:20:02 +0500

    learning

---
+++ VirtualFunction.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Animal
+{
+public:
+    virtual void speak()
+    {
+        cout << "ko ko" << endl;
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    void speak()
+    {
+        cout << "wao wao" << endl;
+    }
+};
+class Cat : public Animal
+{
+public:
+    void speak()
+    {
+        cout << "Meow Meow" << endl;
+    }
+};
+
+int main()
+{
+    Animal *p;
+    vector<Animal*> animals;
+    animals.push_back(new Dog());
+    animals.push_back(new Cat());
+    animals.push_back(new Animal());
+    animals.push_back(new Dog());
+    animals.push_back(new Cat());
+
+    for (int i = 0; i < animals.size(); i++)
+    {
+        p = animals[i];
+        p->speak();
+    }
+}
